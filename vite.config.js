1607604c588ecedf1c5df4a95a0a9364bd4be42b
Santiago Demierre <santidemierre@gmail.com>
2026-04-23 22:18:42 -0300

    Corregir Tailwind y base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/demiweb_app/', // <--- Esta es la línea clave para GitHub Pages
-})
+  base: command === 'serve' ? '/' : '/demiweb_app/',
+}))
